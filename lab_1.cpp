d046550dcf3a3b642ab2a5842e79d69baaab778b
vivivi123098456765-4 <vivivi123098456765@gmail.com>
2026-02-13 01:08:16 +1000

    Added lab_1 code

---
+++ lab_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Node {
+private:
+
+	string content;
+	vector<Node*> children;
+
+public:
+
+	Node(string text) : content(text) {}
+
+	void addChild(Node* childNode) {
+
+		children.push_back(childNode);
+
+	}
+
+};
+
+int main() {
+
+	Node* root = new Node("ROOT");
+
+	Node* subBrunch1 = new Node("wtf");
+	Node* subBrunch2 = new Node("FAAA");
+
+	root->addChild(subBrunch1);
+	root->addChild(subBrunch2);
+
+}
