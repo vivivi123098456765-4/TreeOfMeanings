8230288ae91ec76c4a65c258336643a3cd162749
vivivi123098456765-4 <vivivi123098456765@gmail.com>
2026-02-16 19:59:32 +1000

    Lab 3: Recursive search in N-ary tree
    In this lab, I successfully adapted the TREE-SEARCH algorithm from Cormen's Chapter 12.2. I implemented a recursive search function that traverses the tree and returns a pointer to the target node. During this task, I deepened my understanding of how recursion stacks work and how to correctly propagate a found result back to the root caller using nullptr handling.

---
+++ lab3.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Node {
+private:
+
+	string content;
+	vector<Node*> children;
+
+public:
+
+	string getContent() { return content; }
+
+	Node(string text) : content(text) {}
+
+	Node* findNode(string target) {
+
+		if (content == target) {
+			return this;
+		}
+
+		for (Node* child : children) {
+
+			Node* result = child->findNode(target);
+			if (result != nullptr) { return result; }
+
+		}
+
+		return nullptr;
+	}
+
+	void addChild(Node* childNode) {
+
+		children.push_back(childNode);
+
+	}
+
+	void printTree(int level = 0) {
+
+		for (int i = 0; i < level; i++) {
+			cout << "  ";
+		}
+
+		cout << "|-- " << content << endl;
+
+
+		for (Node* child : children) {
+			child->printTree(level + 1);
+		}
+	}
+
+};
+
+int main() {
+
+	Node* root = new Node("ROOT");
+
+	Node* subBrunch1 = new Node("architecture");
+	Node* subBrunch2 = new Node("design");
+
+	root->addChild(subBrunch1);
+	root->addChild(subBrunch2);
+
+	Node* subBrunch3 = new Node("clean architecture book");
+	Node* subBrunch4 = new Node("Figma Mockups");
+
+
+	subBrunch1->addChild(subBrunch3);
+	subBrunch2->addChild(subBrunch4);
+
+	cout << "Tree structure:" << endl;
+	root->printTree(0);
+
+	cout << "-------------------------------" << endl << endl;
+
+	cout << "Testing search..." << endl;
+
+	string target = "architecture";
+	Node* found = root->findNode(target);
+
+	if (found != nullptr) {
+		cout << "Success! Found node with content: " << found->getContent() << endl;
+	}
+	else {
+		cout << "Sorry, node '" << target << "' not found." << endl;
+	}
+	return 0;
+}
